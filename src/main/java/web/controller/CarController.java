2986a3f60235d38cdf68c5e4223b5854ca25f641
Maxim Chuprygin <pro100nepro2@gmail.com>
2024-03-06 19:47:50 +0300

    change Carcontroller logic

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,24 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarService carService;
+    public CarController(CarService carService) {
+        this.carService= carService;
+
+     }
 
     @GetMapping(value = "/car")
     public String getCars(@RequestParam(value = "count",defaultValue = "5") int allCars, Model model) {
-        List<Car> cars = new ArrayList<>();
-
-        cars.add(new Car(1,"red",100));
-        cars.add(new Car(2,"blue",150));
-        cars.add(new Car(3,"black",250));
-        cars.add(new Car(4,"white",350));
-        cars.add(new Car(5,"green",450));
-        cars = CarService.carsCount(cars,allCars);
-
+        List<Car> cars = carService.createCarList();
+        cars = carService.carsCount(cars,allCars);
         model.addAttribute("cars", cars);
         return "cars";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,15 +1,29 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Component
 public class CarService {
-    public static List<Car> carsCount (List<Car> list, int number) {
+    private final List<Car> cars;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(1,"red",100));
+        cars.add(new Car(2,"blue",150));
+        cars.add(new Car(3,"black",250));
+        cars.add(new Car(4,"white",350));
+        cars.add(new Car(5,"green",450));
+    }
+    public List<Car> createCarList() {
+        return cars;
+    }
+    public  List<Car> carsCount (List<Car> list, int number) {
         if (number <= 0 || number > 5) {
             return list;
         }
-        return  list.stream().limit(number).collect(Collectors.toList());
+        return  list.stream().limit(number).toList();
     }
 }
